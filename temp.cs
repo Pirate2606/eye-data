0b77bdf8697c201ddb08e7d3ba2783583a365fcf
Aditya Naitan <42977085+Pirate2606@users.noreply.github.com>
2023-09-15 15:57:54 +0530

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -1,29 +1,25 @@
-Color32[] colors = new Color32[width * height];
+int width = 0;
+int height = 0;
 
-for (int i = 0; i < height; i++)
+for (int i = 0; i < byteArray.Length - 1; i++)
 {
-    for (int j = 0; j < width; j++)
+    if (byteArray[i] == 0xFF && byteArray[i + 1] == 0xC0)
     {
-        int index = i * width + j;
-        int y = (int)yData[index];
-        int u = (int)uData[index / 4]; // U is usually subsampled
-        int v = (int)vData[index / 4]; // V is usually subsampled
-
-        // Perform YUV to RGB conversion (there are different formulas, use the one you have)
-        int c = y - 16;
-        int d = u - 128;
-        int e = v - 128;
-        int r = (298 * c + 409 * e + 128) >> 8;
-        int g = (298 * c - 100 * d - 208 * e + 128) >> 8;
-        int b = (298 * c + 516 * d + 128) >> 8;
-
-        colors[index] = new Color32((byte)r, (byte)g, (byte)b, 255);
+        // Found the SOF marker (FF C0)
+        // The width and height are stored in the next two bytes (in big-endian order)
+        width = (byteArray[i + 7] << 8) | byteArray[i + 8];
+        height = (byteArray[i + 5] << 8) | byteArray[i + 6];
+        break;
     }
 }
 
-texture.SetPixels32(colors);
-texture.Apply();
-
-Renderer renderer = // Get a reference to your renderer (e.g., SpriteRenderer, MeshRenderer, etc.)
-Material material = renderer.material;
-material.mainTexture = texture;
+if (width > 0 && height > 0)
+{
+    // Width and height have been successfully determined.
+    // You can proceed with image processing.
+}
+else
+{
+    // Dimensions couldn't be determined from the byte array.
+    // Handle this case accordingly.
+}
